52b988db51ae5ae3eeae2eaafa45a4c5c678b15d
namzul629 <nazmulhossain.qnh@gmail.com>
2021-02-20 15:23:23 +0600

    Added all balance and displane in dashboard

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -7,9 +7,25 @@ import { firestoreConnect } from 'react-redux-firebase';
 import Spinner from '../layout/Spinner'
 
 class Clients extends Component {
-  
+    
+    state ={
+        totalOwed:1
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        const { clients } = props;
+
+        if(clients){
+            const total = clients.reduce((total,client)=>{
+                return total+parseFloat(client.balance.toString());
+            }, 0)
+            return { totalOwed: total }
+        }
+        return null
+    }
     render() {
         const  { clients } = this.props;
+       const {totalOwed} = this.state
         if(clients){
             return (
                 <div>
@@ -20,7 +36,12 @@ class Clients extends Component {
                         </div>
                         
                         <div className="col-md-6">
-
+                            <h5 className="text-right text-secondary">
+                                Total Owed:  
+                                <span className="text-primary">
+                                    ${parseFloat(totalOwed).toFixed(2)}
+                                </span>
+                            </h5>
                         </div>
 
                     </div>
